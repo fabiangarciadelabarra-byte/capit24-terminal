b3e7f1206b54da58eb00b38245a25885183516ab
fabiangarciadelabarra-byte <fabiangarciadelabarra@gmail.com>
2026-05-09 15:51:33 -0500

    Create terminal.js

---
+++ pages/terminal.js
@@ -0,0 +1,17 @@
+export async function getServerSideProps() {
+  const res = await fetch("https://capit24-prices.vercel.app/api/all");
+  const data = await res.json();
+
+  return {
+    props: { data },
+  };
+}
+
+export default function TerminalPage({ data }) {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Terminal de Precios</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
